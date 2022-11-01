3cd024c691f1ee8ec3ae85413a74d9d410af4e87
Martinmessner <martinmessner95@gmail.com>
2022-11-01 15:12:37 -0300

    cambios

--- database/connectdb.js
+++ database/connectdb.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 try {
-    await mongoose.connect(process.env.URI_MONGO)
+    await mongoose.connect(process.env.MONGODB_URI)
     console.log("Conectada Exitosamente a la BBDD")
 } catch (error) {
     console.log("Error en la conexion a la base de datos" + error)
